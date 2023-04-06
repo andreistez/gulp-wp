914723e5533df0ff0a01ff26d9a5007339331b92
Andrei Stezenko <andrew.stezenko@gmail.com>
2023-04-06 16:48:59 +0300

    upd: return jQuery maybe will be needed for some plugins on frontend.

--- theme-functions/theme-functions.php
+++ theme-functions/theme-functions.php
@@ -8,11 +8,3 @@
  * @subpackage critick
  */
 
-/**
- * Prevent jQuery scripts from loading on the front-end.
- */
-add_action( 'wp_default_scripts', 'critick_remove_jq_migrate' );
-function critick_remove_jq_migrate( $scripts ){
-	if( ! is_admin() ) $scripts->remove( 'jquery' );
-}
-
